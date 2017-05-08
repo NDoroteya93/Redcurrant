0f75f659489a8b442db99ac6e9d34bf158762930
Doroteya Nikolova <doroteanikolova93@gmail.com>
2017-05-08 21:59:27 +0300

    add filter function for table with use details, fixed jumbotron image - responsive

--- public/Controllers/user-controller.js
+++ public/Controllers/user-controller.js
@@ -147,6 +147,7 @@ class UserController {
             self.createChart(taskStates);
         }, 500)
 
+        this.filter();
         // Events
         $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function(e) {
             let href = e.target.href.split('#')[1];
@@ -158,6 +159,7 @@ class UserController {
         $(document).on("click", ".sidebar-toggle", function() {
             $(".wrapper").toggleClass("toggled");
         });
+
     }
 
     createChart(dataState) {
@@ -195,7 +197,45 @@ class UserController {
                     }
                 }
             });
+    }
 
+    filter() {
+        // attach table filter plugin to inputs
+        $(document).on('keyup', '[data-action="filter"]', function(e) {
+            $('.filterTable_no_results').remove();
+            var $this = $(this),
+                search = $this.val().toLowerCase(),
+                target = $this.attr('data-filters'),
+                $target = $(target),
+                $rows = $target.find('tbody tr');
+
+            if (search == '') {
+                $rows.show();
+            } else {
+                $rows.each(function() {
+                    var $this = $(this);
+                    $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+                })
+                if ($target.find('tbody tr:visible').length === 0) {
+                    var col_count = $target.find('tr').first().find('td').size();
+                    var no_results = $('<tr class="filterTable_no_results"><td colspan="' + col_count + '">No results found</td></tr>')
+                    $target.find('tbody').append(no_results);
+                }
+            }
+        });
+
+
+        $(document).on('click', '.table-user-tickets span.filter', function(e) {
+            var $this = $(this),
+                $panel = $this.parents('.panel');
+
+            $panel.find('.panel-body').slideToggle();
+            if ($this.css('display') != 'none') {
+                $panel.find('.panel-body input').focus();
+            }
+        });
+        $('[data-toggle="tooltip"]').tooltip();
+        // attach table filter plugin to inputs
     }
 }
 
