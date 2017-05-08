5a11b80796ad41c58b981ef442c297b797139299
DilyanaKom <dilyana.komitova@gmail.com>
2017-05-08 20:17:44 +0300

    add tickets model tests

--- public/Tests/tests-setup.js
+++ public/Tests/tests-setup.js
@@ -2,6 +2,7 @@
 'use strict'
 import { requester } from 'requester';
 import { UserModel } from 'userModel';
+import { TicketsModel } from 'ticketsModel';
 
 mocha.setup('bdd');
 
@@ -9,6 +10,7 @@ let expect = chai.expect;
 let assert = chai.assert;
 
 let userModel = new UserModel;
+let ticketsModel = new TicketsModel;
 //let sinon = require('sinon');
 let spyFunction = sinon.spy();
 
@@ -133,7 +135,7 @@ describe('Test Requester and models', function () {
 
         });
 
-         it('expect User model getUser to call get ', (done) => {
+        it('expect User model getUser to call get ', (done) => {
 
             jsonRequesterGetStub.returns(Promise.resolve([]));
 
@@ -144,7 +146,7 @@ describe('Test Requester and models', function () {
 
         });
 
-          it('expect User model getUser to return user ', (done) => {
+        it('expect User model getUser to return user ', (done) => {
 
             const responseUsers =
                 [{
@@ -170,6 +172,223 @@ describe('Test Requester and models', function () {
 
 
         });
+
+
+    });
+
+
+    describe('Test tickets model', function () {
+
+        it('expect Ticket model getTickets to call get request', (done) => {
+
+
+            jsonRequesterGetStub.returns(Promise.resolve([]));
+
+            var result = ticketsModel.getTickets();
+            expect(jsonRequesterGetStub).to.have.been.calledOnce;
+
+            done();
+        });
+
+        it('expect Ticket model getTickets to return tickets separated in groups ', (done) => {
+
+            const responseTasks =
+                [{
+                    taskState: 0
+                },
+                {
+                    taskState: 1
+                },
+                {
+                    taskState: 2
+                },
+                {
+                    taskState: 0
+                }
+                ];
+
+            jsonRequesterGetStub.returns(Promise.resolve(responseTasks));
+
+            var result = ticketsModel.getTickets();
+
+
+            expect(result.todo.length).to.equal(0);
+            expect(result.progress.length).to.equal(0);
+            expect(result.done.length).to.equal(0);
+
+            setTimeout(() => {
+                expect(result.todo.length).to.equal(2);
+                expect(result.progress.length).to.equal(1);
+                expect(result.todo.length).to.equal(1);
+            }, 1000)
+
+            done();
+
+
+
+        });
+
+        it('expect Ticket model changeState to make post request', (done) => {
+
+
+            jsonRequesterPostStub.returns(Promise.resolve({}));
+
+            var result = ticketsModel.changeState();
+            expect(jsonRequesterPostStub).to.have.been.calledOnce;
+
+            done();
+        });
+
+
+        it('expect Ticket model getTicketsDetails to call get request', (done) => {
+
+
+            jsonRequesterGetStub.returns(Promise.resolve([]));
+
+            var result = ticketsModel.getTicketsDetails();
+            expect(jsonRequesterGetStub).to.have.been.calledOnce;
+
+            done();
+        });
+
+        it('expect Ticket model getTicketsDetails to return details', (done) => {
+
+            var res = {
+                createDate: new Date().getDate(),
+                taskState: 0,
+                assignee: ''
+            };
+
+            jsonRequesterGetStub.returns(Promise.resolve(res));
+
+            var result = ticketsModel.getTicketsDetails();
+
+            setTimeout(() => {
+                expect(result.state).to.equal("ToDo");
+                expect(result.assignee).to.equal("Unassigned");
+
+            }, 1000);
+
+            done();
+        });
+
+        it('expect Ticket model addTicket to make post request', (done) => {
+
+
+
+            jsonRequesterPostStub.returns(Promise.resolve({}));
+
+            var result = ticketsModel.addTicket().then(() =>
+            { expect(jsonRequesterPostStub).to.have.been.calledOnce; }
+
+            ).then(done, done);
+
+
+        });
+
+        it('expect Ticket model assigneeUserToTask to make post request', (done) => {
+
+
+
+            jsonRequesterPostStub.returns(Promise.resolve({}));
+
+            var result = ticketsModel.addTicket();
+            expect(jsonRequesterPostStub).to.have.been.calledOnce;
+
+            done();
+
+
+        });
+
+        it('expect Ticket model assigneeUserToTask to return response', (done) => {
+
+            var res = { assignee: "Gosho" }
+
+            jsonRequesterPostStub.returns(Promise.resolve(res));
+
+            var result = ticketsModel.addTicket();
+            expect(jsonRequesterPostStub).to.have.been.calledOnce;
+
+            setTimeout(() => {
+
+                expect(result.assignee).to.equal("Gosho");
+
+            }, 1000);
+
+            done();
+
+
+        });
+
+        it('expect Ticket model deleteTicket to make post request', (done) => {
+
+
+
+            jsonRequesterPostStub.returns(Promise.resolve({}));
+
+            var result = ticketsModel.deleteTicket();
+            expect(jsonRequesterPostStub).to.have.been.calledOnce;
+
+            done();
+
+
+        });
+
+        it('expect Ticket model deleteTicket to return response', (done) => {
+
+            var res = { id: 1 }
+
+            jsonRequesterPostStub.returns(Promise.resolve(res));
+
+            var result = ticketsModel.addTicket();
+            expect(jsonRequesterPostStub).to.have.been.calledOnce;
+
+            setTimeout(() => {
+
+                expect(result.id).to.equal(1);
+
+            }, 1000);
+
+            done();
+
+
+        });
+
+        it('expect Ticket model addComment to make post request', (done) => {
+
+
+
+            jsonRequesterPostStub.returns(Promise.resolve({}));
+
+            var result = ticketsModel.addComment();
+            expect(jsonRequesterPostStub).to.have.been.calledOnce;
+
+            done();
+
+
+        });
+
+        it('expect Ticket model addComment to return response', (done) => {
+
+            var res = { content: "commenting" }
+
+            jsonRequesterPostStub.returns(Promise.resolve(res));
+
+            var result = ticketsModel.addTicket();
+            expect(jsonRequesterPostStub).to.have.been.calledOnce;
+
+            setTimeout(() => {
+
+                expect(result.content).to.equal("commenting");
+
+            }, 1000);
+
+            done();
+
+
+        });
+
+
 
 
     });
